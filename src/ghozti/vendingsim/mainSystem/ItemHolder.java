5a562aba9dff39dfbe9c1e696eeeca6d84b48294
Ghozti <ybalderas1005@gmail.com>
2021-04-05 10:19:33 -0400

    - filled in ItemHolder

--- src/ghozti/vendingsim/mainSystem/ItemHolder.java
+++ src/ghozti/vendingsim/mainSystem/ItemHolder.java
@@ -1,4 +1,17 @@
 package ghozti.vendingsim.mainSystem;
 
+import ghozti.vendingsim.items.Snack;
+
+import java.util.ArrayList;
+
 public class ItemHolder {
+
+    private int itemStock;
+    private final int number;
+    public ArrayList<Snack> snakcs =  new ArrayList<>();
+
+    public ItemHolder(int stock, int number){
+        itemStock = stock;
+        this.number = number;
+    }
 }
