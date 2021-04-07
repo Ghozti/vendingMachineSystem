a8dee5e6587d367fda5deae7ca96243b0b0725cd
Ghozti <ybalderas1005@gmail.com>
2021-04-07 16:07:47 -0400

    - commented more

--- src/ghozti/vendingsim/mainSystem/ItemPusher.java
+++ src/ghozti/vendingsim/mainSystem/ItemPusher.java
@@ -2,6 +2,10 @@ package ghozti.vendingsim.mainSystem;
 
 public class ItemPusher {
 
+    /**
+     *  This method will first check to make sure the stock is not 0 or less (why not)
+     *  then it will remove the last element in the holder's array list and decrement the stock number by 1.
+     */
     public void pushItem(ItemHolder holder){
         if(holder.getStock() <= 0){
             System.err.println("");//TODO add no stock constant
